dcb151e5fea637e75c1ac55ea7a4fbe64442ba30
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-12 17:41:20 -0700

    test flow data set

--- workflows/workflow1/step2.py
+++ workflows/workflow1/step2.py
@@ -3,4 +3,7 @@ import time
 
 def execute(handler):
     print("running workflow1 step2")
+    path = "$.workflow1.step1.field1.field2"
+    value = "hello world"
+    handler.flow_data.set(path, value)
     time.sleep(1)
