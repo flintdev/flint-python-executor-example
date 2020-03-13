c39ec0d85bd66dd7265fe574e5bbd5a450232b78
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-13 15:40:32 -0700

    update workflow

--- workflows/workflow1/step1.py
+++ workflows/workflow1/step1.py
@@ -4,6 +4,6 @@ import time
 def execute(handler):
     print("running workflow1 step1")
     path = "$.workflow1.step1.field1.field2"
-    value = handler.flow_data.get(path)
-    print(value)
+    value = "test1"
+    handler.flow_data.set(path, value)
     time.sleep(1)
--- workflows/workflow1/step2.py
+++ workflows/workflow1/step2.py
@@ -4,6 +4,5 @@ import time
 def execute(handler):
     print("running workflow1 step2")
     path = "$.workflow1.step1.field1.field2"
-    value = "hello world"
-    handler.flow_data.set(path, value)
+    handler.flow_data.get(path)
     time.sleep(1)
