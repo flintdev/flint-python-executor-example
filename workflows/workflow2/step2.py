f8b465c1493c55871c1b38f0c758f5414b749814
Gaoxin Dai <daigx1990@gmmail.com>
2020-03-18 13:58:29 -0700

    update steps

--- workflows/workflow2/step2.py
+++ workflows/workflow2/step2.py
@@ -4,5 +4,6 @@ import time
 def execute(handler):
     print("running workflow2 step2")
     path = "$.workflow2.step1.field1.field2"
-    handler.flow_data.get(path)
+    value = handler.flow_data.get(path)
+    print(path, value)
     time.sleep(1)
